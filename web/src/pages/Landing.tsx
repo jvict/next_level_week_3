c9942410b76b797c81973076902b37d503f93f2f
João Victor <joao.tiburcio@summit-bra.com>
2020-10-15 18:07:04 -0300

    Terminada tela com mapa

---
+++ web/src/pages/Landing.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import '../styles/pages/landing.css'
+import { FiArrowRight } from 'react-icons/fi' ;
+import { Link } from 'react-router-dom'
+import LogoSVG from '../images/Logo.svg';
+
+function Landing() {
+    return (
+        <div id="page-landing">
+
+            <div className="content-wrapper">
+                <img src={LogoSVG} alt="" />
+                <main>
+                    <h1>Leve a felicidade para o mundo</h1>
+                    <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+                </main>
+
+                <div className="location">
+                    <strong>Rio do Sul</strong>
+                    <span>Santa Catarina</span>
+                </div>
+                <Link to="/app" className="enter-app">
+                    <FiArrowRight size={26} color="rgba(0,0,0,0.6)" />
+                </Link>
+            </div>
+
+        </div>
+    )
+}
+
+export default Landing;
